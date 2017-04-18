49837317c6a92bfd7f10196e4c0289549cbe2cb0
Eitan Simler <eitan.simler@gmail.com>
2017-04-17 21:13:11 -0700

    Added SignUpNameActivity

--- app/src/main/java/com/payforward/androidapp/SignUpTypeActivity.java
+++ app/src/main/java/com/payforward/androidapp/SignUpTypeActivity.java
@@ -1,7 +1,9 @@
 package com.payforward.androidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpTypeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SignUpTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_type);
+    }
+
+    public void nextScreen(View view) {
+        Intent intent = new Intent(this, SignUpNameActivity.class);
+        startActivity(intent);
     }
 }
