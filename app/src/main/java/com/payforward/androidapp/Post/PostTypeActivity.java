6ac8cdec0768fe3d72711a291c0e63548c6af3ff
Eitan Simler <eitan.simler@gmail.com>
2017-05-01 15:47:11 -0700

    Created Post Page

---
+++ app/src/main/java/com/payforward/androidapp/Post/PostTypeActivity.java
@@ -0,0 +1,15 @@
+package com.payforward.androidapp.Post;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.payforward.androidapp.R;
+
+public class PostTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_type);
+    }
+}
