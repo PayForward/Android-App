fa0b75a6c6e51cfa40d76cf5d52594d5c5dfffe2
Eitan Simler <eitan.simler@gmail.com>
2017-05-16 13:36:57 -0700

    Added SubCategory files

---
+++ app/src/main/java/com/payforward/androidapp/Post/SubCategory.java
@@ -0,0 +1,29 @@
+package com.payforward.androidapp.Post;
+
+/**
+ * @author Eitan created on 5/16/2017.
+ */
+
+class SubCategory {
+    // Instance Variables
+    private int id;  // Should be final
+    private String description;
+
+    // Constructors
+    public SubCategory(int ID, String description) {
+        id = ID;
+        this.description = description;
+    }
+
+    public SubCategory(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
