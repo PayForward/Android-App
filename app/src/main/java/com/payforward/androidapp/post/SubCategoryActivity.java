ad26abfa583367c685d116c5bdb5c7c608cceabd
Eitan Simler <eitan.simler@gmail.com>
2017-05-17 18:56:43 -0700

    Added back button to action bar for subCategory

--- app/src/main/java/com/payforward/androidapp/post/SubCategoryActivity.java
+++ app/src/main/java/com/payforward/androidapp/post/SubCategoryActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,6 +31,12 @@ public class SubCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_category);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar);
+        // Creates back button
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mSubCategoryList = (ListView) findViewById(R.id.subcategory_list_view);
 
