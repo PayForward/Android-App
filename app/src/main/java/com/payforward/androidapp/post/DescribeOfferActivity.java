da1fd5d91999a41ac88ddbe011726df2cb3ef605
Eitan Simler <eitan.simler@gmail.com>
2017-05-21 13:42:50 -0700

    Changed logo

---
+++ app/src/main/java/com/payforward/androidapp/post/DescribeOfferActivity.java
@@ -0,0 +1,15 @@
+package com.payforward.androidapp.post;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.payforward.androidapp.R;
+
+public class DescribeOfferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_describe_offer);
+    }
+}
