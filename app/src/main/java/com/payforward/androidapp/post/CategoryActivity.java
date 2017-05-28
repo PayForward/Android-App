faf00f5240465b5275b6f4fb3cc8086c376964ba
Eitan Simler <eitan.simler@gmail.com>
2017-05-28 13:35:46 -0700

    Small fixes

--- app/src/main/java/com/payforward/androidapp/post/CategoryActivity.java
+++ app/src/main/java/com/payforward/androidapp/post/CategoryActivity.java
@@ -2,12 +2,10 @@ package com.payforward.androidapp.post;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
