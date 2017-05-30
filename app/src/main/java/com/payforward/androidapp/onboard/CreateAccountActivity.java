6b94d1f3e7e23f5ba8c32bb306c40d2adbb4befe
Eitan Simler <eitan.simler@gmail.com>
2017-05-30 13:42:13 -0700

    Added Email and Password authentication and connection to FireBase

--- app/src/main/java/com/payforward/androidapp/onboard/CreateAccountActivity.java
+++ app/src/main/java/com/payforward/androidapp/onboard/CreateAccountActivity.java
@@ -1,22 +1,31 @@
 package com.payforward.androidapp.onboard;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.payforward.androidapp.R;
 
 public class CreateAccountActivity extends AppCompatActivity {
     private ProgressDialog progress;
     private EditText mEmail;
     private EditText mPassword;
+    private FirebaseAuth mAuth;
+    private final String TAG = "CreateAccountActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +36,11 @@ public class CreateAccountActivity extends AppCompatActivity {
         progress = new ProgressDialog(this);
         progress.setProgress(10);
 
+        // Set EditTexts
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -42,8 +54,17 @@ public class CreateAccountActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        // TODO: Implement this
+        // FirebaseUser currentUser = mAuth.getCurrentUser();
+        // updateUI(currentUser);
+    }
+
     /**
-     * Atempts to proceed to next activity. If it fails, there are
+     * Attempts to proceed to next activity. If it fails, there are
      * errors in the form.
      */
     private void attemptProceed() {
@@ -55,14 +76,25 @@ public class CreateAccountActivity extends AppCompatActivity {
         String email = mEmail.getText().toString();
         String password = mPassword.getText().toString();
 
-        // Check if phone number valid
+        // Check if email valid
         if (TextUtils.isEmpty(email)) {
-            mEmail.setError(getString(R.string.email_required));
-            focusView = mPhoneNumberView;
+            mEmail.setError(getString(R.string.required_error));
+            focusView = mEmail;
             cancel = true;
-        } else if (phoneNumber.length() < 10) {
-            mPhoneNumberView.setError(getString(R.string.error_invalid_phone));
-            focusView = mPhoneNumberView;
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError(getString(R.string.invalid_email_error));
+            focusView = mEmail;
+            cancel = true;
+        }
+
+        // Check if password valid
+        else if (TextUtils.isEmpty(password)) {
+            mPassword.setError(getString(R.string.required_error));
+            focusView = mPassword;
+            cancel = true;
+        } else if (password.length() < 6) {
+            mPassword.setError(getString(R.string.password_length_error));
+            focusView = mPassword;
             cancel = true;
         }
 
@@ -71,12 +103,28 @@ public class CreateAccountActivity extends AppCompatActivity {
             // form field with an error.
             focusView.requestFocus();
         } else {
-            // Go to Different Activity
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                // updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.d(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(CreateAccountActivity.this, task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                                // updateUI(null);
+                            }
+                        }
+                    });
         }
     }
 
-    public void newActivity(View view) {
-        Intent intent = new Intent(this, VerifyMemberActivity.class);
-        startActivity(intent);
+    public void fabClicked(View view) {
+        attemptProceed();
     }
 }
