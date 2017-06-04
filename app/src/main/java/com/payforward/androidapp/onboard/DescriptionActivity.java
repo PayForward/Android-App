d872e923ddc873286fa2a36e23c949e51c8c57ca
jinchizhou <jinchizhou@yahoo.com>
2017-06-04 14:31:53 -0700

    cleaned

--- app/src/main/java/com/payforward/androidapp/onboard/DescriptionActivity.java
+++ app/src/main/java/com/payforward/androidapp/onboard/DescriptionActivity.java
@@ -19,43 +19,16 @@ public class DescriptionActivity extends AppCompatActivity {
 
     private ProgressBar mProgress;
     private TextInputLayout value;
-//    private EditText edittext;
-//    private View v;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
         mProgress = (ProgressBar) findViewById(R.id.progressBar8);
         mProgress.setProgress(40);
-//        edittext = (EditText)findViewById(R.id.desc);
         value = (TextInputLayout)findViewById(R.id.textInputLayout);
     }
-//    public void setupFloatingLabelError(){
-//        final TextInputLayout floating = (TextInputLayout)findViewById(R.id.textInputLayout);
-//        floating.getEditText().addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                if(s.length() > 20){
-//                    CharSequence text = "You have overwritten amount of chars";
-//                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-//                }
-//                else{
-//                    newActivity(v);
-//                    startActivity(new Intent(DescriptionActivity.this, UploadPhotoActivity.class));
-//                }
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//
-//            }
-//        });
-//    }
+
     public void newActivity(View view)
     {
         int length = value.getEditText().length();
