aab44d9d2ef398a8d9eb02c51b93ea5fa5300b53
jinchizhou <jinchizhou@yahoo.com>
2017-06-04 16:00:46 -0700

    working on describe offer

--- app/src/main/java/com/payforward/androidapp/post/DescribeOfferActivity.java
+++ app/src/main/java/com/payforward/androidapp/post/DescribeOfferActivity.java
@@ -2,14 +2,17 @@ package com.payforward.androidapp.post;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.payforward.androidapp.R;
 
 public class DescribeOfferActivity extends AppCompatActivity {
-
+    private ProgressBar mProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_describe_offer);
+        mProgress = (ProgressBar) findViewById(R.id.progressBar11);
+        mProgress.setProgress(65);
     }
 }
