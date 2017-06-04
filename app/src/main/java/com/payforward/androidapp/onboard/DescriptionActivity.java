499a0aab9790a4e6706e4fec4479b22342f02f50
jinchizhou <jinchizhou@yahoo.com>
2017-06-03 18:41:57 -0700

    kb

--- app/src/main/java/com/payforward/androidapp/onboard/DescriptionActivity.java
+++ app/src/main/java/com/payforward/androidapp/onboard/DescriptionActivity.java
@@ -6,18 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.payforward.androidapp.R;
 
 public class DescriptionActivity extends AppCompatActivity {
 
     private ProgressBar mProgress;
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
         mProgress = (ProgressBar) findViewById(R.id.progressBar8);
         mProgress.setProgress(40);
+        int charcounter = 250;
+        tv = (TextView)findViewById(R.id.counter);
+        tv.setText(String.format(Integer.toString(charcounter)));
     }
     public void newActivity(View view)
     {
